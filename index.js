3d3c68d3a8c15e6be5f96d0417f03d134f824672
Ruben <141916508+rubenaguilardev@users.noreply.github.com>
2025-03-26 02:36:39 -0700

    program completed

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+const meterInFeet = 3.28084
+const literInGallon = 0.264172
+const kilogramInPound = 2.20462
 
-let input = document.getElementById('unit-input')
+const input = document.getElementById('unit-input')
+const button = document.getElementById('convert-btn')
 
-console.log(inputValue)
+const lengthEl = document.getElementById('lengthConversion')
+const volumeEl = document.getElementById('volumeConversion')
+const massEl = document.getElementById('massConversion')
+
+button.addEventListener('click', conversion) 
+
+function conversion() {
+    const value = input.value
+
+    let feetCon = value * meterInFeet
+    let meters = value / meterInFeet
+    lengthEl.textContent = `${value} meters = ${feetCon.toFixed(3)} feet |
+     ${value} feet = ${meters.toFixed(3)} meters`
+    
+    let gallons = value * literInGallon
+    let liters = value / literInGallon
+    volumeEl.textContent = `${value} liters = ${gallons.toFixed(3)} gallons |
+     ${value} gallons = ${liters.toFixed(3)} liters`
+
+    let pounds = value * kilogramInPound
+    let kilograms = value / kilogramInPound
+    massEl.textContent = `${value} kilos = ${pounds.toFixed(3)} pounds |
+     ${value} pounds = ${kilograms.toFixed(3)} kilos`
+    
+}
